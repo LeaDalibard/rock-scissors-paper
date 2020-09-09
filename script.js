2359d077c1b42052a7cba3f498ab7f317fa83039
Lea Dalibard <leadalibard@gmail.com>
2020-09-09 16:34:37 +0200

    Basic R/P/S game working, need to make reset

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
 (function(){
 
     let player;
-    let computer;
     let pick=["rock","paper","scissors"];
 
     rock.addEventListener("click", function (){
-        player= pick[0];
+        player= "rock";
     });
 
     paper.addEventListener("click", function (){
-        player= pick[1];
+        player= "paper";
     });
 
     scissors.addEventListener("click", function (){
-        player= pick[3];
+        player= "scissors";
     });
 
     play.addEventListener("click", function(){
-        let random = Math.floor(Math.random()*Math.floor(3))
-
-        computer=pick[random];
-        if (computer == player){}
+        let computer = Math.floor(Math.random()*Math.floor(3))
+        let computerPick= pick[computer]
+        let result=document.getElementById("result")
+        let playerIndex=pick.indexOf(player)
+        if (computer === playerIndex){result.innerHTML= "The computer picked : "+computerPick+". Ex-aequo, try again !"}
+        else if (computer < playerIndex && playerIndex!==2){result.innerHTML="The computer picked : "+computerPick+". Congrats, you win!"}
+        else if (computer!==1){result.innerHTML= "The computer picked : "+computerPick+". Congrats, you win!"}
+        else {result.innerHTML="The computer picked : "+computerPick+". Too bad, you loose. Try again !"}
     })
 
 
