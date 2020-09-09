b26be4cd39b3494d58bed58ebb1c48d3585d76d6
Lea Dalibard <leadalibard@gmail.com>
2020-09-09 16:38:38 +0200

    Button reset working

--- script.js
+++ script.js
@@ -1,32 +1,40 @@
-(function(){
+(function () {
 
     let player;
-    let pick=["rock","paper","scissors"];
+    let pick = ["rock", "paper", "scissors"];
 
-    rock.addEventListener("click", function (){
-        player= "rock";
+    rock.addEventListener("click", function () {
+        player = "rock";
     });
 
-    paper.addEventListener("click", function (){
-        player= "paper";
+    paper.addEventListener("click", function () {
+        player = "paper";
     });
 
-    scissors.addEventListener("click", function (){
-        player= "scissors";
+    scissors.addEventListener("click", function () {
+        player = "scissors";
     });
 
-    play.addEventListener("click", function(){
-        let computer = Math.floor(Math.random()*Math.floor(3))
-        let computerPick= pick[computer]
-        let result=document.getElementById("result")
-        let playerIndex=pick.indexOf(player)
-        if (computer === playerIndex){result.innerHTML= "The computer picked : "+computerPick+". Ex-aequo, try again !"}
-        else if (computer < playerIndex && playerIndex!==2){result.innerHTML="The computer picked : "+computerPick+". Congrats, you win!"}
-        else if (computer!==1){result.innerHTML= "The computer picked : "+computerPick+". Congrats, you win!"}
-        else {result.innerHTML="The computer picked : "+computerPick+". Too bad, you loose. Try again !"}
+    play.addEventListener("click", function () {
+        let computer = Math.floor(Math.random() * Math.floor(3))
+        let computerPick = pick[computer]
+        let result = document.getElementById("result")
+        let playerIndex = pick.indexOf(player)
+        if (computer === playerIndex) {
+            result.innerHTML = "The computer picked : " + computerPick + ". Ex-aequo, try again !"
+        } else if (computer < playerIndex && playerIndex !== 2) {
+            result.innerHTML = "The computer picked : " + computerPick + ". Congrats, you win!"
+        } else if (computer !== 1) {
+            result.innerHTML = "The computer picked : " + computerPick + ". Congrats, you win!"
+        } else {
+            result.innerHTML = "The computer picked : " + computerPick + ". Too bad, you loose. Try again !"
+        }
     })
 
+    reset.addEventListener("click", function (){
+        computer="";
+        player="";
+        result.innerHTML ="";
+    })
 
-
-
-}) ();
+})();
